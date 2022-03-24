14237468f8bc8ca271eca9a54d3a39d63b03e90b
Zac Olden <zacolden@gmail.com>
2022-03-24 19:11:02 +1100

    Add Vite build to resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games {'gta5', 'rdr3'}
+
+files {
+    'dist/index.html',
+    'dist/index.js',
+    'dist/assets/*'
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    }
+  }
 })
